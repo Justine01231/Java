9b06a61fd5da2606c4f26dcdf52116152b47a44e
Justine01231 <solitary.echoes369@gmail.com>
2025-09-18 10:13:44 +0800

    introduction to java

---
+++ intoduction/Main.java
@@ -0,0 +1,17 @@
+package intoduction;
+
+public class Main {
+
+    public static void main (String[] args){
+
+        // syntax for println          so that the next output will be in the next line
+
+        /*
+        this is multi line comment
+        // - this is also a comment
+         */
+
+        System.out.println("Hello World");
+        System.out.println("hello bro");
+    }
+}
